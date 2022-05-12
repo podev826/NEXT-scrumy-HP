da03845895c3efdb15603b2d5309d86f8cd3963a
ring1996 <ring199623@gmail.com>
2022-05-12 23:35:09 +0900

    [update] ContentWrapperコンポーネントに背景色をつけられるようにした

--- frontend/src/components/Layouts/Wrapper/ContentWrapper.tsx
+++ frontend/src/components/Layouts/Wrapper/ContentWrapper.tsx
@@ -2,22 +2,33 @@ import { Box, ContainerProps } from '@chakra-ui/react';
 import { ReactNode, VFC } from 'react';
 
 type ContentWrapperProps = ContainerProps & {
+  bgName?: 'gray' | 'white' | 'dotted';
   children: ReactNode;
 };
 
 export const ContentWrapper: VFC<ContentWrapperProps> = ({
+  bgName = 'gray',
   children,
   ...props
 }) => {
   return (
     <Box
-      maxW={1440}
-      px={{ base: 6, md: 10, lg: 16, xl: 24 }}
-      py={{ base: 16, md: 20, xl: 24 }}
-      mx="auto"
-      {...props}
+      bg={bgName === 'white' ? 'base.100' : 'gray.100'}
+      backgroundImage={
+        bgName === 'dotted'
+          ? "url('/images/common/common_bg_dotted.png')"
+          : undefined
+      }
     >
-      {children}
+      <Box
+        maxW={1440}
+        px={{ base: 6, md: 10, lg: 16, xl: 24 }}
+        py={{ base: 16, md: 20, xl: 24 }}
+        mx="auto"
+        {...props}
+      >
+        {children}
+      </Box>
     </Box>
   );
 };
