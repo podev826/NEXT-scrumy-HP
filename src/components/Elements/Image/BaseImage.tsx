ad2f8bd942869f8b91535b57a13f66a9437e5f97
ring1996 <ring199623@gmail.com>
2022-05-17 15:24:14 +0900

    [add] BaseImageのコンポーネントを実装した

---
+++ src/components/Elements/Image/BaseImage.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import Image, { ImageProps } from 'next/image';
+import { FC } from 'react';
+
+export const BaseImage: FC<ImageProps> = ({ src, alt, width, height }) => {
+  return (
+    <Box fontSize={0}>
+      <Image src={src} width={width} height={height} alt={alt ? alt : ''} />
+    </Box>
+  );
+};
--- src/components/Elements/index.ts
+++ src/components/Elements/index.ts
@@ -1,3 +1,4 @@
 export * from './Icon';
+export * from './Image';
 export * from './Link';
 export * from './Title';
