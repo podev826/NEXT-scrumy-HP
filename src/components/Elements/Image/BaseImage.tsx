ae781475b5cbf8500bb1e0badc8298d68d58b519
ring1996 <ring199623@gmail.com>
2022-05-19 18:14:19 +0900

    [update] BaseImageにpropsを持たせられるようにした

--- src/components/Elements/Image/BaseImage.tsx
+++ src/components/Elements/Image/BaseImage.tsx
@@ -2,10 +2,22 @@ import { Box } from '@chakra-ui/react';
 import Image, { ImageProps } from 'next/image';
 import { FC } from 'react';
 
-export const BaseImage: FC<ImageProps> = ({ src, alt, width, height }) => {
+export const BaseImage: FC<ImageProps> = ({
+  src,
+  alt,
+  width,
+  height,
+  ...props
+}) => {
   return (
     <Box fontSize={0}>
-      <Image src={src} width={width} height={height} alt={alt ? alt : ''} />
+      <Image
+        src={src}
+        width={width}
+        height={height}
+        alt={alt ? alt : ''}
+        {...props}
+      />
     </Box>
   );
 };
