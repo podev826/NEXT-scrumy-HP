29a2e636eaaede1206d893e7e76f89b50c05be14
ring1996 <ring199623@gmail.com>
2022-05-19 18:16:44 +0900

    [update] colorsが分かりやすいようにコメントを付与した

--- src/styles/theme/foundations/colors.ts
+++ src/styles/theme/foundations/colors.ts
@@ -1,16 +1,20 @@
 export const colors = {
   base: {
+    /* white */
     100: '#FFFFFF',
   },
   main: {
+    /* yellow */
     100: '#FFCC3C',
     200: '#FFDC5A',
   },
   sub: {
+    /* black */
     100: '#1D2828',
     200: '#7B7B7B',
   },
   accent: {
+    /* green */
     100: '#38B5A6',
     200: '#447777',
     300: '#38D0B8',
