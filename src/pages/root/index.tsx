76039d2dad6981fca5cbcdc7118ad2fad4bffd77
ring1996 <ring199623@gmail.com>
2022-05-19 10:56:57 +0900

    [update] Topページの仮の表示を変更した

--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -1,63 +1,21 @@
 import { Box, Text } from '@chakra-ui/react';
-import {
-  AboutSubVisual,
-  BaseLink,
-  PrimaryLink,
-  PrimarySubVisual,
-  PrimaryTitle,
-  PrivacyPolicySubVisual,
-  SecondaryLink,
-} from 'components/Elements';
+import { PrimaryLink, PrimaryTitle } from 'components/Elements';
 import { ContentWrapper, MainLayout } from 'components/Layouts';
-import { SUB_VISUAL_LIST } from 'configs';
 import type { NextPage } from 'next';
 
 const Root: NextPage = () => {
   return (
     <MainLayout>
-      <AboutSubVisual />
-      <Box mt={10}>
-        <PrimarySubVisual content={SUB_VISUAL_LIST.recruit} />
-      </Box>
-      <Box mt={10}>
-        <PrivacyPolicySubVisual />
-      </Box>
-      <ContentWrapper as="section" bgType="dotted">
-        <PrimaryTitle ja="注目の記事" en="Pick up" />
-        <Box>
-          <Text mt={10}>
-            <BaseLink href="/about">BaseLink</BaseLink>
-          </Text>
-          <Text mt={10}>
-            <PrimaryLink href="/" variant="primary" w="full" maxW="400">
-              問い合わせをする
-            </PrimaryLink>
-          </Text>
-          <Text mt={10}>
-            <PrimaryLink href="/" variant="secondary" w="full" maxW="400">
-              詳しくみる
-            </PrimaryLink>
-          </Text>
-          <Text mt={10}>
-            <PrimaryLink href="/" variant="tertiary" w="full" maxW="400">
-              文字数が多い時のデザインデザインデザイン
-            </PrimaryLink>
-          </Text>
-          <Text mt={10}>
-            <SecondaryLink href="/" variant="tertiary" w="full" maxW="400">
-              問い合わせをする
-            </SecondaryLink>
-          </Text>
-          <Text mt={10}>
-            <SecondaryLink href="/" variant="primary" w="full" maxW="400">
-              詳しくみる
-            </SecondaryLink>
-          </Text>
-          <Text mt={10}>
-            <SecondaryLink href="/" variant="secondary" w="full" maxW="400">
-              文字数が多い時のデザインデザインデザイン
-            </SecondaryLink>
-          </Text>
+      <ContentWrapper as="section" bgType="dotted" h="100vh">
+        <Box pt="120">
+          <PrimaryTitle ja="注目の記事" en="Pick up" />
+          <Box>
+            <Text mt={10}>
+              <PrimaryLink href="/" variant="primary" w="full" maxW="400">
+                問い合わせをする
+              </PrimaryLink>
+            </Text>
+          </Box>
         </Box>
       </ContentWrapper>
     </MainLayout>
