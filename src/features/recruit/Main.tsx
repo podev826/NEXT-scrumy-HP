d204562428a780347ebb59899c7c89051066ae96
ring1996 <ring199623@gmail.com>
2022-05-30 16:06:07 +0900

    [update] Recruitページのアコーディオンが動作するようにした

--- src/features/recruit/Main.tsx
+++ src/features/recruit/Main.tsx
@@ -1,6 +1,7 @@
-import { Box, Grid, Text } from '@chakra-ui/react';
+import { Accordion, AccordionItem, Box, Grid, Text } from '@chakra-ui/react';
 import {
-  CircleMinusIcon,
+  BaseAccordionButton,
+  BaseAccordionPanel,
   PrimaryLink,
   PrimaryTitle,
 } from 'components/Elements';
@@ -11,48 +12,43 @@ import { FC } from 'react';
 type JobDescriptionDetailProps = {
   term: string;
   description: string;
-  last: boolean;
 };
 
 const JobDescriptionDetail: FC<JobDescriptionDetailProps> = ({
   term,
   description,
-  last,
 }) => {
   return (
     <Box
       fontWeight={'bold'}
-      pb={last ? { base: 0, xl: 6 } : { base: 6 }}
+      pb={{ base: 6 }}
       position="relative"
-      _after={
-        last
-          ? {
-              xl: {
-                content: '""',
-                display: 'block',
-                width: '100%',
-                height: '1px',
-                backgroundColor: 'sub.100',
-                opacity: '0.16',
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                right: 0,
-              },
-            }
-          : {
-              content: '""',
-              display: 'block',
-              width: '100%',
-              height: '1px',
-              backgroundColor: 'sub.100',
-              opacity: '0.16',
-              position: 'absolute',
-              bottom: 0,
-              left: 0,
-              right: 0,
-            }
-      }
+      _after={{
+        content: '""',
+        display: 'block',
+        width: '100%',
+        height: '1px',
+        backgroundColor: 'sub.100',
+        opacity: '0.16',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+      _last={{
+        pb: {
+          base: 0,
+          md: 6,
+        },
+        '::after': {
+          base: {
+            opacity: 0,
+          },
+          md: {
+            opacity: '0.16 !important',
+          },
+        },
+      }}
     >
       <Text>{term}</Text>
       <Text mt={2}>{description}</Text>
@@ -65,77 +61,51 @@ export const JobDescriptionContent: FC<JobDescriptionContentProps> = ({
   content,
 }) => {
   return (
-    <Box boxShadow={'secondary'} borderRadius="32px" overflow={'hidden'}>
-      <Box
-        as="dt"
-        bg="sub.200"
-        px={{ base: 8, xl: 14 }}
-        py={{ base: 6, xl: 9 }}
-      >
-        <Box display={{ xl: 'flex' }} alignItems={{ xl: 'center' }}>
-          <Text
-            bg="main.100"
-            display={'inline-block'}
-            fontSize={{ base: 'sm', xl: 'md' }}
-            fontWeight={'bold'}
-            py={1}
-            px={3}
-            mr={{ xl: 16 }}
-            minW={{ xl: '140px' }}
-            textAlign="center"
-          >
-            募集職種
-          </Text>
-          <Box
-            fontSize={{ base: 'lg', xl: 'xl' }}
-            color="base.100"
-            fontWeight={'bold'}
-            alignItems="center"
-            display={{ base: 'flex' }}
-            justifyContent={'space-between'}
-            gap={{ base: 2, xl: 10 }}
-            flexGrow={{ xl: 1 }}
-            mt={{ base: 3, xl: 0 }}
-          >
-            <Text>{title}</Text>
-            <Grid
-              w={{ base: 5, xl: 10 }}
-              h={{ base: 5, xl: 10 }}
-              fontSize={{ base: 'xl', xl: '5xl' }}
-              color="accent.100"
-              alignItems={'center'}
-              justifyContent={'center'}
-              as="button"
-              transitionProperty="all"
-              transitionTimingFunction="ease-out"
-              transitionDuration="fast"
-              _hover={{
-                opacity: 0.7,
-              }}
-            >
-              <CircleMinusIcon />
+    <AccordionItem
+      key={title}
+      boxShadow={'secondary'}
+      borderRadius="32px"
+      overflow="hidden"
+      border="none"
+      p={0}
+    >
+      {({ isExpanded }) => (
+        <>
+          <BaseAccordionButton isExpanded={isExpanded}>
+            <Box display={{ xl: 'flex' }} alignItems={{ xl: 'center' }}>
+              <Text
+                bg="main.100"
+                color="sub.100"
+                display={'inline-block'}
+                fontSize={{ base: 'sm', xl: 'md' }}
+                fontWeight={'bold'}
+                py={1}
+                px={3}
+                mr={{ xl: 16 }}
+                minW={{ xl: '140px' }}
+                textAlign="center"
+              >
+                募集職種
+              </Text>
+              <Box pr={{ base: 3, xl: 6 }} mt={{ base: 3, xl: 0 }}>
+                {title}
+              </Box>
+            </Box>
+          </BaseAccordionButton>
+          <BaseAccordionPanel>
+            <Grid w="full" maxW="880px" gap={{ base: 6 }}>
+              {content.map((item) => (
+                <JobDescriptionDetail
+                  key={item.term}
+                  term={item.term}
+                  description={item.description}
+                />
+              ))}
             </Grid>
-          </Box>
-        </Box>
-      </Box>
-      <Box
-        as="dd"
-        px={{ base: 8, xl: 14 }}
-        py={{ base: 8, xl: 24 }}
-        bg="base.100"
-      >
-        <Grid w="full" maxW="880px" gap={{ base: 6 }}>
-          {content.map((item, index) => (
-            <JobDescriptionDetail
-              key={item.term}
-              term={item.term}
-              description={item.description}
-              last={index + 1 === content.length ? true : false}
-            />
-          ))}
-        </Grid>
-      </Box>
-    </Box>
+          </BaseAccordionPanel>
+        </>
+      )}
+    </AccordionItem>
   );
 };
 
@@ -144,7 +114,13 @@ export const RecruitMain: FC = () => {
     <ContentWrapper bgType="gray">
       <PrimaryTitle ja="募集要項" en="Recruit" />
       <Box mt={{ base: 8, md: 12, xl: 16 }} pl={{ xl: 14 }}>
-        <Grid as="dl" gap={{ base: 6, xl: 12 }}>
+        <Accordion
+          as="dl"
+          allowToggle
+          allowMultiple
+          display={'grid'}
+          gap={{ base: 5, xl: 8 }}
+        >
           {JOB_DESCRIPTION.map((item) => (
             <JobDescriptionContent
               key={item.title}
@@ -152,7 +128,7 @@ export const RecruitMain: FC = () => {
               content={item.content}
             />
           ))}
-        </Grid>
+        </Accordion>
         <Box mt={{ base: 8, xl: 10 }}>
           <PrimaryLink
             href="/contact"
--- src/features/recruit/Member.tsx
+++ src/features/recruit/Member.tsx
@@ -1,5 +1,17 @@
-import { Box, Text } from '@chakra-ui/react';
-import { BaseImage, CircleMinusIcon, PrimaryTitle } from 'components/Elements';
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Text,
+} from '@chakra-ui/react';
+import {
+  BaseImage,
+  CircleMinusIcon,
+  CirclePlusIcon,
+  PrimaryTitle,
+} from 'components/Elements';
 import { ContentWrapper } from 'components/Layouts';
 import { MEMBER_LIST } from 'configs/Recruit';
 import { FC } from 'react';
@@ -13,7 +25,9 @@ export const RecruitMember: FC = () => {
         pl={{ xl: 14 }}
         overflowX={{ base: 'scroll' }}
       >
-        <Box
+        <Accordion
+          allowToggle
+          allowMultiple
           w={{ base: 'calc((240px * 6) + (12px * 5))', xl: 'full' }}
           display={{ base: 'flex', xl: 'grid' }}
           gridTemplateColumns={{ xl: 'repeat(3, 1fr)' }}
@@ -22,62 +36,85 @@ export const RecruitMember: FC = () => {
           overflowX={{ base: 'scroll' }}
         >
           {MEMBER_LIST.map((item) => (
-            <Box
+            <AccordionItem
               key={item.name}
               w={{ base: '240px', xl: 'full' }}
               overflow={'hidden'}
             >
-              <BaseImage src={item.image} width={726} height={726} alt="" />
-              <Box
-                bg="base.100"
-                px={{ base: 7, xl: 8 }}
-                borderBottomRadius={{ base: '16px' }}
-              >
-                <Box py={{ base: 6, xl: 5 }}>
-                  <Box bg="sub.100" display={'inline-block'} px={3}>
-                    <Text
-                      color={'base.100'}
-                      fontWeight="bold"
-                      fontSize={{ base: 'sm', xl: 'md' }}
-                      lang="en"
-                    >
-                      {item.position}
-                    </Text>
-                  </Box>
-                  <Box
-                    display={{ base: 'flex' }}
-                    justifyContent={{ base: 'space-between' }}
-                    alignItems={{ base: 'flex-end' }}
-                    flexGrow={{ xl: 1 }}
-                    mt={{ base: 3 }}
+              {({ isExpanded }) => (
+                <Box
+                  w="full"
+                  bg="base.100"
+                  borderBottomRadius={{ base: '16px' }}
+                >
+                  <BaseImage src={item.image} width={726} height={726} alt="" />
+                  <AccordionButton
+                    p={0}
+                    _hover={{ bg: 'base.100' }}
+                    _focus={{ outline: 'none' }}
                   >
-                    <Text fontSize={{ base: 'lg', xl: 'xl' }} fontWeight="bold">
-                      {item.name}
-                    </Text>
-                    <Box
-                      as="button"
-                      fontSize={{ base: 'xl', xl: '2xl' }}
-                      color={'accent.100'}
-                      display="flex"
-                      alignItems={'flex-end'}
-                      transitionProperty="all"
-                      transitionTimingFunction="ease-out"
-                      transitionDuration="fast"
-                      _hover={{
-                        opacity: 0.7,
-                      }}
-                    >
-                      <CircleMinusIcon />
+                    <Box px={{ base: 7, xl: 8 }} w="full">
+                      <Box
+                        py={{ base: 6, xl: 5 }}
+                        display={{ base: 'flex' }}
+                        flexDirection="column"
+                        alignItems={'flex-start'}
+                      >
+                        <Box bg="sub.100" display={'inline-block'} px={3}>
+                          <Text
+                            color={'base.100'}
+                            fontWeight="bold"
+                            fontSize={{ base: 'sm', xl: 'md' }}
+                            lang="en"
+                          >
+                            {item.position}
+                          </Text>
+                        </Box>
+                        <Box
+                          w="full"
+                          display={{ base: 'flex' }}
+                          justifyContent={{ base: 'space-between' }}
+                          alignItems={{ base: 'flex-end' }}
+                          flexGrow={{ xl: 1 }}
+                          mt={{ base: 3 }}
+                        >
+                          <Text
+                            fontSize={{ base: 'lg', xl: 'xl' }}
+                            fontWeight="bold"
+                          >
+                            {item.name}
+                          </Text>
+                          <Box
+                            as="button"
+                            fontSize={{ base: 'xl', xl: '2xl' }}
+                            color={'accent.100'}
+                            display="flex"
+                            alignItems={'flex-end'}
+                            transitionProperty="all"
+                            transitionTimingFunction="ease-out"
+                            transitionDuration="fast"
+                            _hover={{
+                              opacity: 0.7,
+                            }}
+                          >
+                            {isExpanded ? (
+                              <CircleMinusIcon />
+                            ) : (
+                              <CirclePlusIcon />
+                            )}
+                          </Box>
+                        </Box>
+                      </Box>
                     </Box>
-                  </Box>
+                  </AccordionButton>
+                  <AccordionPanel pb={8} px={{ base: 7, xl: 8 }}>
+                    <Text>{item.description}</Text>
+                  </AccordionPanel>
                 </Box>
-                <Box pb={8} display="none">
-                  <Text>{item.description}</Text>
-                </Box>
-              </Box>
-            </Box>
+              )}
+            </AccordionItem>
           ))}
-        </Box>
+        </Accordion>
       </Box>
     </ContentWrapper>
   );
