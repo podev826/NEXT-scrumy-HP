f58e66b9ec0d94ba401fabfde49926aaf9cddc7e
ring1996 <ring199623@gmail.com>
2022-06-10 11:36:52 +0900

    [update] 自動返信メールをSendGridのテンプレートで管理するようにした

--- src/pages/api/sendgrid/sendEmail.ts
+++ src/pages/api/sendgrid/sendEmail.ts
@@ -12,16 +12,39 @@ const sendEmail = (
   if (req.method === 'POST') {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-    const message = {
-      to: req.body.email,
-      from: process.env.SENDGRID_FROM_EMAIL_ADDRESS,
-      subject: 'お問い合わせありがとうございます',
-      text: `${req.body.company_name} 様\n\nこの度はお問い合わせいただき、誠にありがとうございます。\n以下の内容でお問い合わせを受付いたしました。\n\nお問い合わせの種類\n${req.body.contact_type}\nお問い合わせ内容\n${req.body.content}\n\nお問い合わせ内容については、後ほど、担当者よりご連絡をさせていただきます。\n今しばらくお待ちくださいますようよろしくお願い申し上げます。`,
+    const contact_type = req.body.contact_type || '未回答';
+    const company_name = req.body.company_name || '未回答';
+    const person_name = req.body.person_name || '未回答';
+    const tel = req.body.tel || '未回答';
+    const content = req.body.content || '未回答';
+
+    const data = {
+      from: {
+        name: '株式会社Scrumy',
+        email: process.env.SENDGRID_FROM_EMAIL_ADDRESS,
+      },
+      personalizations: [
+        {
+          to: [
+            {
+              email: req.body.email,
+            },
+          ],
+          dynamic_template_data: {
+            contact_type: contact_type,
+            company_name: company_name,
+            person_name: person_name,
+            tel: tel,
+            content: content,
+          },
+        },
+      ],
+      templateId: 'd-c1ac08115803404885cb28eaa9052d5e',
     };
 
     (async () => {
       try {
-        await sgMail.send(message);
+        await sgMail.send(data);
         res.status(200).end();
       } catch (error) {
         res.status(500).end();
