c69bff2758f38ee18e9b25759c97d906db7a3093
ring1996 <ring199623@gmail.com>
2022-06-16 00:14:51 +0900

    [kari] 本番環境でしか確認できないエラーの確認作業8

--- src/features/root/News.tsx
+++ src/features/root/News.tsx
@@ -52,7 +52,8 @@ export const RootNews: FC<RootNewsProps> = ({ contents }) => {
                             mr={{ xl: 16 }}
                           >
                             <Text mr={{ base: 5, xl: 10 }} lang="en">
-                              {dayjs(item.publishedAt).format('YYYY.MM.DD')}
+                              days
+                              {/* {dayjs(item.publishedAt).format('YYYY.MM.DD')} */}
                             </Text>
                             <Text
                               minW={{ base: 20, xl: '140px' }}
