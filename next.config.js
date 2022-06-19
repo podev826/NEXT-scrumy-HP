a79a0f455b09e56342fdb877ce153fad69ca5930
ring1996 <ring199623@gmail.com>
2022-06-19 18:56:49 +0900

    [fix] next.config.jsにキャッシュに関する記述を削除した

--- next.config.js
+++ next.config.js
@@ -6,19 +6,6 @@ const nextConfig = {
   images: {
     domains: ['images.microcms-assets.io'],
   },
-  async headers() {
-    return [
-      {
-        source: '/images/about/about_spacer.jpg',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=2592000, stale-while-revalidate=86400',
-          },
-        ],
-      },
-    ];
-  },
   reactStrictMode: true,
 };
 
