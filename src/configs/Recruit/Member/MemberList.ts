a1cbf98c78dc363efedb5cacfbf41e77d344518b
keisukeM0R1T4 <keisuke.m0510@gmail.com>
2023-01-04 00:21:08 +0900

    [fix]メンバーのpositionの修正

--- src/configs/Recruit/Member/MemberList.ts
+++ src/configs/Recruit/Member/MemberList.ts
@@ -16,28 +16,28 @@ export const MEMBER_LIST = [
   {
     name: '並木 健剛',
     image: '/images/about/Kengo Namiki 1.png',
-    position: 'Manager',
+    position: 'CEO',
     description:
       '神奈川県出身。慶應義塾大学経済学部卒。株式会社三井住友銀行にて財務や営業に従事。現在はD2Cブランド制作を支援する株式会社Far East InnovatorsではCOOとして全社統括しており、M&Aに向けて挑戦中。',
   },
   {
     name: '江崎　大航',
     image: '/images/about/Taiko Ezaki 1.png',
-    position: 'Intern',
+    position: 'CSO',
     description:
       '東京都出身。慶應義塾大学医学部在学中。医療情報学や医療政策学を勉強中。「データ情報の循環」をかかげる代表笹埜の思想に強く共感し、株式会社Scrumyのメンバーとして加わる。',
   },
   {
     name: '藤澤 諒祐',
     image: '/images/about/fuzisawa.png',
-    position: 'Legal Volunteer',
+    position: 'CLO',
     description:
       '岡山県出身。京都大学法学部卒。司法修習生（74期）。現在、法律事務所インターンとして、企業法務のほか、システム・ソフトウェア・アプリ開発に関する契約関連やIT訴訟（システム開発訴訟や特許訴訟など）のIT法務を経験中。',
   },
   {
     name: '國領　二郎',
     image: '/images/about/Jiro Kokuryo.png',
-    position: 'aaaa',
+    position: 'Adviser',
     description: 'aaaaaaaaaaaaaaaa',
   },
 ];
