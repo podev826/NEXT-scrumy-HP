c3dcda4ba0beb05ef7b165dc94bd4eee7ac1a553
kohei <koehi3400@gmail.com>
2023-01-04 22:28:59 +0900

    [update]wとplを変更

--- src/features/recruit/CEO.tsx
+++ src/features/recruit/CEO.tsx
@@ -17,12 +17,12 @@ import { FC } from 'react';
 
 export const RecruitCEO: FC = () => {
   return (
-    <Box pl={{ base: 6, md: 5, lg: 12 }}>
+    <Box pl={{ base: 6, md: 5, lg: 12, xl: 20 }}>
       <FadeInAnimation>
         <Accordion allowToggle>
           <AccordionItem
             key={MEMBER_LIST[0].name}
-            w={{ base: '350px', xl: '28vw' }}
+            w={{ base: '350px', xl: '370px' }}
             _last={{
               borderBottomWidth: 0,
             }}
