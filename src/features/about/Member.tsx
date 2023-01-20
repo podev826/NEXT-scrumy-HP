b574a4ecbccd2e66a9f5816b801412830fcc7353
kohei <koehi3400@gmail.com>
2023-01-20 20:13:22 +0900

    [update]デザインを変更

--- src/features/about/Member.tsx
+++ src/features/about/Member.tsx
@@ -33,103 +33,102 @@ export const AboutMember: FC = () => {
               allowMultiple
               w={{ base: 'calc((240px * 6) + (12px * 5))', xl: 'full' }}
               display={{ base: 'flex', xl: 'grid' }}
+              pb="1px"
               gridTemplateColumns={{ xl: 'repeat(2, 1fr)' }}
               rowGap={{ base: 3, md: 4, xl: '5vw' }}
               columnGap={{ base: 3, md: 4, xl: '5vw' }}
               overflowX={{ base: 'scroll', xl: 'visible' }}
             >
               {MEMBER_LIST.map((item) => (
-                <Box key={item.name} mb="1px">
-                  <AccordionItem
-                    w={{ base: '240px', xl: 'full' }}
-                    overflow={'hidden'}
-                    shadow="lg"
-                    borderRadius="30px"
-                    mb={{ base: 'auto', xl: 'auto' }}
-                  >
-                    {({ isExpanded }) => (
-                      <Box>
-                        <Box w="full" display={{ base: '', xl: 'flex' }}>
-                          <Box>
-                            <Image src={item.image} width="300px" alt="" />
-                          </Box>
-                          <AccordionButton
-                            p={0}
-                            _hover={{ bg: 'base.100' }}
-                            _focus={{ outline: 'none' }}
-                          >
-                            <Box px={{ base: 4, xl: 4 }} w="full">
-                              <Box
-                                py={{ base: 6, xl: 5 }}
-                                display={{ base: 'flex' }}
-                                flexDirection="column"
-                                alignItems={'flex-start'}
-                              >
-                                <Box display={'inline-block'}>
-                                  <Text
-                                    fontWeight="bold"
-                                    fontSize={{ base: 'sm', xl: 'md' }}
-                                  >
-                                    {item.position}
-                                  </Text>
-                                </Box>
-                                <Box display={'inline-block'} mt={3}>
-                                  <Text
-                                    fontWeight="bold"
-                                    fontSize={{
-                                      base: 'sm',
-                                      lg: 'xl',
-                                      xl: '2vw',
-                                    }}
-                                  >
-                                    {item.name_en}
-                                  </Text>
-                                </Box>
-                                <Box
-                                  w="full"
-                                  display={{ base: 'flex' }}
-                                  justifyContent={{ base: 'space-between' }}
-                                  alignItems={{ base: 'flex-end' }}
-                                  flexGrow={{ xl: 1 }}
-                                  mt={{ base: 3 }}
+                <AccordionItem
+                  key={item.name}
+                  w={{ base: '240px', xl: 'full' }}
+                  overflow={'hidden'}
+                  shadow="lg"
+                  borderRadius="30px"
+                  mb={{ base: 'auto', xl: 'auto' }}
+                >
+                  {({ isExpanded }) => (
+                    <Box>
+                      <Box w="full" display={{ base: '', xl: 'flex' }}>
+                        <Box>
+                          <Image src={item.image} width="300px" alt="" />
+                        </Box>
+                        <AccordionButton
+                          p={0}
+                          _hover={{ bg: 'base.100' }}
+                          _focus={{ outline: 'none' }}
+                        >
+                          <Box px={{ base: 4, xl: 4 }} w="full">
+                            <Box
+                              py={{ base: 6, xl: 5 }}
+                              display={{ base: 'flex' }}
+                              flexDirection="column"
+                              alignItems={'flex-start'}
+                            >
+                              <Box display={'inline-block'}>
+                                <Text
+                                  fontWeight="bold"
+                                  fontSize={{ base: 'sm', xl: 'md' }}
                                 >
-                                  <Text
-                                    fontSize={{ base: 'lg', xl: 'xl' }}
-                                    fontWeight="bold"
-                                  >
-                                    {item.name}
-                                  </Text>
-                                  <Box
-                                    fontSize={{ base: 'xl', xl: '2xl' }}
-                                    color={'accent.100'}
-                                    position="relative"
-                                    top="5"
-                                    left="1"
-                                    transitionProperty="all"
-                                    transitionTimingFunction="linear"
-                                    transitionDuration="fast"
-                                    _hover={{
-                                      opacity: 0.7,
-                                    }}
-                                  >
-                                    {isExpanded ? (
-                                      <CircleMinusIcon />
-                                    ) : (
-                                      <CirclePlusIcon />
-                                    )}
-                                  </Box>
+                                  {item.position}
+                                </Text>
+                              </Box>
+                              <Box display={'inline-block'} mt={3}>
+                                <Text
+                                  fontWeight="bold"
+                                  fontSize={{
+                                    base: 'xl',
+                                    xl: '2vw',
+                                  }}
+                                >
+                                  {item.name_en}
+                                </Text>
+                              </Box>
+                              <Box
+                                w="full"
+                                display={{ base: 'flex' }}
+                                justifyContent={{ base: 'space-between' }}
+                                alignItems={{ base: 'flex-end' }}
+                                flexGrow={{ xl: 1 }}
+                                mt={{ base: 3 }}
+                              >
+                                <Text
+                                  fontSize={{ base: 'lg', xl: 'xl' }}
+                                  fontWeight="bold"
+                                >
+                                  {item.name}
+                                </Text>
+                                <Box
+                                  fontSize={{ base: 'xl', xl: '2xl' }}
+                                  color={'accent.100'}
+                                  position="relative"
+                                  top="5"
+                                  left="1"
+                                  transitionProperty="all"
+                                  transitionTimingFunction="linear"
+                                  transitionDuration="fast"
+                                  _hover={{
+                                    opacity: 0.7,
+                                  }}
+                                >
+                                  {isExpanded ? (
+                                    <CircleMinusIcon />
+                                  ) : (
+                                    <CirclePlusIcon />
+                                  )}
                                 </Box>
                               </Box>
                             </Box>
-                          </AccordionButton>
-                        </Box>
-                        <AccordionPanel pb={8} px={{ base: 7, xl: 8 }}>
-                          <Text>{item.description}</Text>
-                        </AccordionPanel>
+                          </Box>
+                        </AccordionButton>
                       </Box>
-                    )}
-                  </AccordionItem>
-                </Box>
+                      <AccordionPanel pb={8} px={{ base: 7, xl: 8 }}>
+                        <Text>{item.description}</Text>
+                      </AccordionPanel>
+                    </Box>
+                  )}
+                </AccordionItem>
               ))}
             </Accordion>
           </Box>
