e0ff8960024bc9f717249b720e8152f492aef412
keisukeM0R1T4 <105028540+keisukeM0R1T4@users.noreply.github.com>
2023-01-28 20:26:25 +0900

    [fix]カテゴリー表記の修正

--- src/components/Elements/Algolia/CustomHits.tsx
+++ src/components/Elements/Algolia/CustomHits.tsx
@@ -1,5 +1,4 @@
 import { Accordion, AccordionItem, Box, Text } from '@chakra-ui/react';
-import { NEWS_CATEGORIES } from 'configs';
 import dayjs from 'dayjs';
 import React, { FC } from 'react';
 import { HitsProps, useHits } from 'react-instantsearch-hooks-web';
@@ -55,28 +54,8 @@ export const CustomHits: FC<
                       px={2}
                       textAlign={'center'}
                     >
-                      {hit.category} {/* ここを変更する */}
+                      {hit.category}
                     </Text>
-                    <Box
-                      minW={{ base: 20, xl: '140px' }}
-                      display="inline-block"
-                      bg="main.100"
-                      color="sub.100"
-                      px={2}
-                      textAlign={'center'}
-                    >
-                      {hit.category === NEWS_CATEGORIES.all.slug
-                        ? NEWS_CATEGORIES.all.text
-                        : hit.category === NEWS_CATEGORIES.information.slug
-                        ? NEWS_CATEGORIES.information.text
-                        : hit.category === NEWS_CATEGORIES.pressRelease.slug
-                        ? NEWS_CATEGORIES.pressRelease.text
-                        : hit.category === NEWS_CATEGORIES.seminar.slug
-                        ? NEWS_CATEGORIES.seminar.text
-                        : hit.category === NEWS_CATEGORIES.eBook.slug
-                        ? NEWS_CATEGORIES.eBook.text
-                        : null}
-                    </Box>
                   </Box>
                   <Box pr={{ base: 3, xl: 6 }} mt={{ base: 3, xl: 0 }}>
                     {hit.title}
