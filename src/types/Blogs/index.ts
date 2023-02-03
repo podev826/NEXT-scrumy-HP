f1dfca76f77865706af9d94a42474526cee4680a
keisukeM0R1T4 <keisuke.m0510@gmail.com>
2023-02-03 17:18:59 +0900

    [update]型定義の更新

--- src/types/Blogs/index.ts
+++ src/types/Blogs/index.ts
@@ -10,10 +10,17 @@ export type BlogItemProps = {
   title: string;
   content: string;
   description: string;
-  eyecatch: HTMLImageElement;
+  image: MicroCMSDate & BlogEyecatchDataProps;
   category: MicroCMSDate & BlogCategoryDataProps;
+  publishedAt: string;
+  objectID: string;
 };
 
+export type BlogEyecatchDataProps = {
+  url: string;
+  height: string;
+  width: string;
+};
 export type BlogContentProps = BlogItemProps & MicroCMSContentId & MicroCMSDate;
 
 export type BlogCategoryDataProps = {
@@ -21,4 +28,15 @@ export type BlogCategoryDataProps = {
   name: string;
 };
 
+export type ContentType = {
+  id: string;
+};
+
+export type ContextType = {
+  params: ContextParamsType;
+};
+
+export type ContextParamsType = {
+  id: string;
+};
 export type BlogDataProps = MicroCMSListResponse<BlogItemProps>;
