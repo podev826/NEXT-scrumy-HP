7d02cd8f5bedb88423d9b0be457004408681aa5c
kohei <koehi3400@gmail.com>
2023-02-11 16:46:50 +0900

    [update]目次の開閉を追加

--- src/features/Blogs/TableOfContent.tsx
+++ src/features/Blogs/TableOfContent.tsx
@@ -1,5 +1,5 @@
 import { Box, Link, Text } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { TocType } from 'types';
 
 type TocTypeProps = {
@@ -9,32 +9,32 @@ type TocTypeProps = {
 export const TableOfContents: FC<TocTypeProps> = ({ toc }) => {
   let h2 = 0;
   let h3 = 0;
+  const [isOpen, setIsOpen] = useState<boolean>(true);
   return (
-    <Box
-      my="20"
-      border="1px"
-      borderColor="#38B5A6"
-      w="fit-content"
-      p="6"
-      mx="auto"
-    >
-      <Box display="flex" justifyContent={'center'}>
-        <Text className="TableOfContentsHead" mr="3">
-          目次
-        </Text>
-        <Text cursor={'pointer'}>[閉じる]</Text>
+    <Box my="20" border="1px" borderColor="#38B5A6" w="fit-content" mx="auto">
+      <Box display="flex" justifyContent={'center'} p="2">
+        <Box mr="5">目次</Box>
+        <Box cursor={'pointer'} onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? '[閉じる]' : '[開く]'}
+        </Box>
       </Box>
-      <Box mt="5">
-        {toc.map((data: TocType) => (
-          <Text key={data.id} cursor={'pointer'}>
-            <Link href={`#${data.text}`} ml={data.name == 'h3' ? '4' : 0}>
-              {data.name == 'h2'
-                ? String((h2 += 1)) + '.'
-                : String((h3 += 1)) + '.'}{' '}
-              {data.text}
-            </Link>
-          </Text>
-        ))}
+      <Box pb={5} px="7" display={isOpen ? 'block' : 'none'}>
+        <Box mt="3">
+          {toc.map((data: TocType) => (
+            <Text key={data.id}>
+              <Link
+                href={`#${data.text}`}
+                ml={data.name == 'h3' ? '4' : 0}
+                cursor={'pointer'}
+              >
+                {data.name == 'h2'
+                  ? String((h2 += 1)) + '.'
+                  : String((h3 += 1)) + '.'}
+                {data.text}
+              </Link>
+            </Text>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
