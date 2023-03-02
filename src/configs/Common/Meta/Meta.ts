32b1133dd3af5ab6d77347212465014f2e9eb6cf
keisukeM0R1T4 <105028540+keisukeM0R1T4@users.noreply.github.com>
2023-03-02 10:55:07 +0900

    [fix]メタディスクリプションの変更

--- src/configs/Common/Meta/Meta.ts
+++ src/configs/Common/Meta/Meta.ts
@@ -3,25 +3,25 @@ export const META = {
     href: '/',
     title: '株式会社Scrumy',
     description:
-      '株式会社Scrumyは、サステイナブル経営のためのESG情報開示戦略をサポートする会社です。ESG情報開示戦略コンサル「Scrumy」・会社設立サポート「スクラム申請」の運営などの業務に携わっております。',
+      '株式会社Scrumy（スクラミー）は、サステナブルファイナンス領域の専門家集団です。ESG経営コンサルティングサービスや、非財務情報の開示ツールを提供しております。',
   },
   about: {
     href: '/about/',
     title: 'About us | 株式会社Scrumy',
     description:
-      '株式会社ScrumyのAbout usページです。株式会社Scrumyは、サステイナブル経営のためのESG情報開示戦略をサポートする会社です。',
+      '株式会社Scrumy（スクラミー）の会社紹介ページです。当社のビジョンや世界観、メンバーについてご紹介いたします。',
   },
   news: {
     href: '/news/',
     title: 'News | 株式会社Scrumy',
     description:
-      '株式会社ScrumyのNewsページです。株式会社Scrumyは、サステイナブル経営のためのESG情報開示戦略をサポートする会社です。',
+      '株式会社Scrumy（スクラミー）のニュースページです。当社の取り組みや最新のニュースについてお知らせいたします。',
   },
   recruit: {
     href: '/recruit/',
     title: 'Recruit | 株式会社Scrumy',
     description:
-      '株式会社ScrumyのRecruitページです。株式会社Scrumyは、サステイナブル経営のためのESG情報開示戦略をサポートする会社です。',
+      '株式会社Scrumy（スクラミー）の採用ページです。採用要件などをお伝えしております。',
   },
   contact: {
     href: '/contact/',
