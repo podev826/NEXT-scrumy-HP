85a23ab1573d7c918f5470c93fb3bac7c4a75b8c
kohei <koehi3400@gmail.com>
2023-03-17 15:22:46 +0900

    fix

--- src/libraries/microcms/client.ts
+++ src/libraries/microcms/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const Newsclient = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN1,
-  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY1,
+  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
 });
 
 // export const Blogclient = createClient({
